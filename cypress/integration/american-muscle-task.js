39bf5b0657ff19b70c74ad40464888f1b398a40a
Yasmeen Tak <yasmeen.tak.33@gmail.com>
2021-12-15 00:43:16 +0200

    Shop carmaro 2016-2022  and rotors done

--- cypress/integration/american-muscle-task.js
+++ cypress/integration/american-muscle-task.js
@@ -1,7 +1,8 @@
 /// <reference types="cypress"/>
+
 describe('American Muscle Add to cart Scenario with specific filtering', () => {
   before(() => {
-    cy.visit('/');
+    cy.visit('');
   });
 
   beforeEach(() => {
@@ -13,6 +14,68 @@ describe('American Muscle Add to cart Scenario with specific filtering', () => {
   });
 
   context('Home page', () => {
-    it('Verify home screen after visit the website', () => {});
+    it('Verify Home Page after visit the website for first time', () => {
+      cy.title().should(
+        'contain',
+        'Mustang Parts & Accessories | AmericanMuscle'
+      );
+      cy.wait(3000);
+      cy.get('.vehicle .vehicle_select_container nav')
+        .should('be.visible')
+        .and('contain', 'Shop Camaro');
+      //   cy.get('.vehicle .vehicle_select_container nav').should(
+      //     'have.attr',
+      //     'a[data-vehicle-type="Camaro"]'
+      //   );
+    });
+    it('Verify "Shop Camaro" have the active class', () => {
+      cy.get('.vehicle nav .camaro_trigger').realHover();
+      cy.get('.vehicle nav .camaro_trigger span').should(
+        'have.css',
+        'color',
+        'rgb(245, 130, 31)'
+      );
+    });
+    it('Verify Navigate to "Shop Camaro" form the slider', () => {
+      cy.get('.vehicle nav .camaro_trigger').click();
+      cy.url().should('include', 'camaro');
+    });
+    //-----------------------------------------
+    it('Verify "2016-2022" have the active class', () => {
+      cy.get('.camaro a[href*="/2016-camaro"]').realHover();
+      // .vehicle_select_container nav a:hover span
+      cy.get('.camaro a[href*="/2016-camaro"] span').should(
+        'have.css',
+        'color',
+        'rgb(245, 130, 31)'
+      );
+    });
+    it('Verify Clicking on "2016-2022" from home page Main Banner', () => {
+      cy.get('.camaro a.back')
+        .should('be.visible')
+        .and('contain', 'Back to Vehicles');
+      cy.get('.camaro a[href*="/2016-camaro"]').click();
+      cy.url().should('include', '2016-camaro-accessories');
+    });
+    //-----------------------------------------
+  });
+
+
+  after(() => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
   });
 });
+
+// - Navigate to https://www.americanmuscle.com/
+// - Choose Camaro Vehicle (2016-2022) from the home page's Main Banner
+// - Select 'Brakes -> Rotor' category from the top-nav.
+// - Close the auto loaded modal.
+// - Filter to see items in 'Brake Rotors and Drums' Category.
+// - Filter to see items only have price between $120 and $290
+// - Sort the items based on the customer rating.
+// - Open product details page for the prodcut with the highest customer rating.
+// - Add the product to the saved products list for test email in this syntax "yourfirstname.yourlastname@test.com" (example:anas.salahat@test.com).
+// - Navigate to my build (saved products page) from the header -> my account dropdown.
+// - Add the selected product to the cart.
+// - Modify the quantity from the cart to be 11.
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,23 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import 'cypress-real-events/support';
+
+// Check for real work app onboarding dialog
+
+Cypress.Commands.add('marketingModalForFirstTime', () => {
+  //   cy.wait(2000);
+  //   cy.get('body').then((el) => {
+  //     const element = el.find('.overlay.marketing_modal').length;
+  //     if (element) {
+  //       cy.get('a.close_trigger').click();
+  //     }
+  //   });
+  const $el = Cypress.$('.overlay.marketing_modal');
+  cy.wait(6000);
+  if ($el.length) {
+    cy.log('Closing modal');
+    cy.get('a.close_trigger').click();
+  }
+  cy.get('.overlay.marketing_modal').should('not.be.visible');
+});
