e0fafe91e5c1d22707a166b8107ee903c7f4036d
Yasmeen Tak <yasmeen.tak.33@gmail.com>
2021-12-16 07:59:03 +0200

    edit add readme file

--- cypress/integration/american-muscle-task.js
+++ cypress/integration/american-muscle-task.js
@@ -264,7 +264,6 @@ describe('American Muscle Add to cart Scenario with specific filtering', () => {
       });
     });
     it('Verify after navigate to "Saved Products" Page', () => {
-      //التحقق من انو العنصر نفس البيانات
       cy.get('.build_list p').should('contain', '1');
       cy.get('.products_container li').should('contain', 'In Stock');
     });
@@ -322,15 +321,3 @@ describe('American Muscle Add to cart Scenario with specific filtering', () => {
   });
 });
 
-// - Navigate to https://www.americanmuscle.com/
-// - Choose Camaro Vehicle (2016-2022) from the home page's Main Banner
-// - Select 'Brakes -> Rotor' category from the top-nav.
-// - Close the auto loaded modal.
-// - Filter to see items in 'Brake Rotors and Drums' Category.
-// - Filter to see items only have price between $120 and $290
-// - Sort the items based on the customer rating.
-// - Open product details page for the prodcut with the highest customer rating.
-// - Add the product to the saved products list for test email in this syntax "yourfirstname.yourlastname@test.com" (example:anas.salahat@test.com).
-// - Navigate to my build (saved products page) from the header -> my account dropdown.
-// - Add the selected product to the cart.
-// - Modify the quantity from the cart to be 11.
